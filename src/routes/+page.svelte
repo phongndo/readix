46610e1b09434a3ae78d83710e8fa829893c1661
Phong Do <Donguyenphong@icloud.com>
2025-12-07 01:16:22 -0800

    added supabase dependency for sql db and auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+  h1 {
+    color: #ff3e00;
+  }
+</style>
