5037fb5c459f78997c3374ff60627e6098f72a5a
Phong Do <Donguyenphong@icloud.com>
2025-12-07 23:46:50 -0800

    holy shit i suck at ui/ux...

---
+++ src/lib/components/auth/loginForm.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  let email: string = '';
+  let password: string = '';
+
+</script>
+
+<svelte:head>
+  <title>Login</title>
+</svelte:head>
+
+<section class="flex flex-col min-h-screen justify-center items-center space-y-2">
+  <h1 class="font-bold">Login</h1>
+  <button type="submit" class="bg-primary-600 hover:bg-primary-800 text-white font-bold py-2 px-4">Login with Google</button>
+  <button type="submit" class="bg-primary-600 hover:bg-primary-800 text-white font-bold py-2 px-4">Login with GitHub</button>
+  <hr class="hr border-t-2" />
+  <div>
+    <input id="email" name="email" type="email" required class="text-gray-950" bind:value={email} placeholder="Email">
+  </div>
+  <div>
+    <input id="password" name="password" type="password" required class="text-gray-950" bind:value={password} placeholder="Password">
+  </div>
+  <button type="submit" class="bg-primary-600 hover:bg-primary-800 text-white font-bold py-2 px-4">Login</button>
+</section>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,12 +13,12 @@ import { goto } from '$app/navigation';
 </svelte:head>
 
 <main>
-  <div class="flex flex-col min-h-screen justify-center items-center space-y-8">
-    <h1 class="font-bold text-4xl">Welcome to <span class="font-bold text-orange-500">Readix</span></h1>
+  <div class="flex flex-col min-h-screen justify-center items-center space-y-4">
+    <h1 class="font-bold text-4xl">Welcome to <span class="font-bold text-primary-600">Readix</span></h1>
     <p class="mt-4 text-lg text-center max-w-md">
-      A performance-focused reading platform engineered for comprehension mastery. 
+      A pdf reading platform
     </p>
-    <button type="button" class="bg-orange-500 hover:bg-orange-700 text-white fond-bold py-2 px-4" on:click={navigateToDashboard}>
+    <button type="button" class="bg-primary-600 hover:bg-primary-800 font-bold py-2 px-4" on:click={navigateToDashboard}>
       Start your journey 
     </button>
   </div>
