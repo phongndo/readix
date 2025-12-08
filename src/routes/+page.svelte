49088d8228fa44d90d63d8f189ede3a8a99e54a5
Phong Do <Donguyenphong@icloud.com>
2025-12-07 19:23:55 -0800

    created a simple landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
-import PDFViewer from '$lib/components/viewers/PDFViewer.svelte';
+import { goto } from '$app/navigation';
+
+  function navigateToDashboard() {
+    goto('/dashboard');
+  }
 </script>
 
-<PDFViewer />
+<svelte:head>
+  <title>Readix</title>
+  <meta name="description" content="A performance-focused reading platform engineered for comprehension mastery." />
+  <link rel="icon" href="/favicon.ico" />
+</svelte:head>
+
+<main>
+  <div class="flex flex-col min-h-screen justify-center items-center space-y-8">
+    <h1 class="font-bold text-4xl">Welcome to <span class="font-bold text-orange-500">Readix</span></h1>
+    <p class="mt-4 text-lg text-center max-w-md">
+      A performance-focused reading platform engineered for comprehension mastery. 
+    </p>
+    <button type="button" class="bg-orange-500 hover:bg-orange-700 text-white fond-bold py-2 px-4" on:click={navigateToDashboard}>
+      Start your journey 
+    </button>
+  </div>
+</main>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts"></script>
+
+<main>
+  <h1 class="text-shadow-gray-300">Hello World</h1>
+</main>
