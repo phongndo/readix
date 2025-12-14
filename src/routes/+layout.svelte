ce50df52d380be2d71f48ecbc0ddaaad639e8bc8
Phong Do <Donguyenphong@icloud.com>
2025-12-14 00:27:02 -0800

    added clerk auth for github and google

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import type { Snippet } from 'svelte';
+	import { ClerkProvider } from 'svelte-clerk';
 
-	let { children } = $props();
+	const { children }: { children: Snippet } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-{@render children()}
+<ClerkProvider>
+	{@render children()}
+</ClerkProvider>
